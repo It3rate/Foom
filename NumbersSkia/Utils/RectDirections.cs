e1061b7991084cd89ad2b7e1f26186ad44b302c8
Robin Debreuil <debreuil.robin@gmail.com>
2025-08-20 10:38:16 -0500

    added rect rotations and ready for side b rotation calculations.

--- NumbersSkia/Utils/RectDirections.cs
+++ NumbersSkia/Utils/RectDirections.cs
@@ -57,9 +57,62 @@ public static class RectDirectionsExtensions
 	public static bool HasLeft(this RectDirections state) => (state & RectDirections.Left) != 0;
 	public static bool HasRight(this RectDirections state) => (state & RectDirections.Right) != 0;
 	public static bool HasTop(this RectDirections state) => (state & RectDirections.Top) != 0;
-	public static bool HasBottom(this RectDirections state) => (state & RectDirections.Bottom) != 0;
+    public static bool HasBottom(this RectDirections state) => (state & RectDirections.Bottom) != 0;
+    public static bool IsCorner(this RectDirections state) => 
+        state == RectDirections.BottomLeft || state == RectDirections.BottomRight || 
+        state == RectDirections.TopLeft || state == RectDirections.TopRight;
 
-	public static SKPoint DirectionToCenter(this RectDirections state, int xRightDirection = 1, int yDownDirection = 1)
+    public static bool CornersFormEdge(this RectDirections state, RectDirections otherCorner)
+    {
+        var result = false;
+        if (state.IsCorner() && otherCorner.IsCorner())
+        {
+            result = (state & otherCorner) > 0;
+        }
+        return result;
+    }
+    public static bool CornersFormDiagonal(this RectDirections state, RectDirections otherCorner)
+    {
+        var result = false;
+        if (state.IsCorner() && otherCorner.IsCorner())
+        {
+            result = (state & otherCorner) == 0;
+        }
+        return result;
+    }
+    public static int Clockwise45DegreeStepsTo(this RectDirections start, RectDirections end)
+    {
+        int startIndex = GetDirectionIndex(start);
+        int endIndex = GetDirectionIndex(end);
+
+        return (endIndex - startIndex + 8) % 8;
+    }
+    public static int CounterClockwise45DegreeStepsTo(this RectDirections start, RectDirections end)
+    {
+        int startIndex = GetDirectionIndex(start);
+        int endIndex = GetDirectionIndex(end);
+
+        return (startIndex - endIndex + 8) % 8;
+    }
+
+    public static int CWDegreesTo(this RectDirections start, RectDirections end)
+    {
+        return start.Clockwise45DegreeStepsTo(end) * 45;
+    }
+    public static float CWRadiansTo(this RectDirections start, RectDirections end)
+    {
+        return start.Clockwise45DegreeStepsTo(end) * 45 * MathF.PI / 180f;
+    }
+    public static int CCWDegreesTo(this RectDirections start, RectDirections end)
+    {
+        return start.CounterClockwise45DegreeStepsTo(end) * 45;
+    }
+    public static float CCWRadiansTo(this RectDirections start, RectDirections end)
+    {
+        return start.CounterClockwise45DegreeStepsTo(end) * 45 * MathF.PI / 180f;
+    }
+
+    public static SKPoint DirectionToCenter(this RectDirections state, int xRightDirection = 1, int yDownDirection = 1)
     {
         var result = SKPoint.Empty;
 
@@ -102,5 +155,20 @@ public static class RectDirectionsExtensions
                 break;
         }
         return result;
+    }
+    public  static int GetDirectionIndex(this RectDirections state)
+    {
+        return state switch
+        {
+            RectDirections.Top => 0,
+            RectDirections.TopRight => 1,
+            RectDirections.Right => 2,
+            RectDirections.BottomRight => 3,
+            RectDirections.Bottom => 4,
+            RectDirections.BottomLeft => 5,
+            RectDirections.Left => 6,
+            RectDirections.TopLeft => 7,
+            _ => 0
+        };
     }
 }
